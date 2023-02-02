5192cf08234b5c6957ab986d602d081d3f41b062
stormshaper45 <123022397+stormshaper45@users.noreply.github.com>
2023-02-02 11:21:42 -0500

    Update Stamps.java

--- src/Stamps.java
+++ src/Stamps.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Stamps {
+/**
+ * class: ITEC 2140
+ * date:01/27/23
+ * aurthur:Joshua Wilson
+ * descrpition: This program is to determine how many more stamps does Jean have then Susan
+ */
+public class Stamps {
+    public static void main(String[] args) {
+        int Jean_Stamps = 40;   // These are setting the amount of the stamps
+        int Susan_Stamps = 8;
+        // the best way to figure out the differences in the stamps is to subtract the greatest number by the smallest
+        int Differences = Jean_Stamps - Susan_Stamps;
+        System.out.print(" The differences in stamps between Jean ans Susan is " + Differences + " stamps");
+    }
 }
